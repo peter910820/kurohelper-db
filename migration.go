af9846b0e5f6356df76038f067f8838b89a37451
SeaotterMS <peter910820@gmail.com>
2025-10-18 14:24:18 +0800

    chore: adjust migration

--- migration.go
+++ migration.go
@@ -1,18 +1,24 @@
 package kurohelperdb
 
 import (
-	"gorm.io/gorm"
+	"errors"
 
 	"github.com/peter910820/kurohelper-db/models"
 )
 
-func Migration(db *gorm.DB) {
-	db.AutoMigrate(&models.ZhtwToJp{})
-	db.AutoMigrate(&models.SeiyaCorrespond{})
-	db.AutoMigrate(
+func Migration() error {
+	if Dbs == nil {
+		return errors.New("DB not initialized")
+	}
+
+	Dbs.AutoMigrate(&models.ZhtwToJp{})
+	Dbs.AutoMigrate(&models.SeiyaCorrespond{})
+	Dbs.AutoMigrate(
 		&models.User{},
 		&models.BrandErogs{},
 		&models.GameErogs{},
 		&models.UserGameErogs{},
 	)
+
+	return nil
 }
